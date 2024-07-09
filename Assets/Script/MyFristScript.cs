5b1162b182ff2e95beeb675a2e4a8c78b09f8803
Kurie <flykuro211@gmail.com>
2024-07-09 11:15:11 +0700

    UpdateProject

---
+++ Assets/Script/MyFristScript.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Threading;
+using UnityEngine;
+using static Unity.Burst.Intrinsics.X86;
+
+namespace Assets.Script
+{
+    public class MyFristScript : MonoBehaviour
+    {
+        public int Tuoi = 1;
+
+        public float A;
+        public float B;
+        public float C;
+        
+        public string Name;
+
+        void Start()
+        {
+            float Delta;
+
+            Delta = B * B - 4 * A * C;
+
+            float X1;
+            float X2;
+            
+
+            if (Delta < 0)
+            {
+                Debug.Log("Vo Nghiem");
+
+            }
+
+            else if (Delta == 0)
+            {
+                X1=X2=-B/(2*A);
+
+                Debug.Log($"Phuong trinh co nghiem kep {X1},{X2}");
+
+            }
+
+            else
+            {
+                X1 = ((float)(-B + Math.Sqrt(Delta)))/(2*A);
+                X2= ((float)(-B - Math.Sqrt(Delta))) / (2 * A);
+
+                Debug.Log($"Phuong trinh co 2 nghiem {X1},{X2}");
+            }
+        
+        }
+
+       
+    }
+}
