4ac1dabbe8195825f47754ffbfaaeb227643dde1
Kurie <flykuro211@gmail.com>
2024-07-20 10:36:33 +0700

    For

--- Assets/Script/MyFirstScript.cs
+++ Assets/Script/MyFirstScript.cs
@@ -7,29 +7,62 @@ namespace Assets.Scripts
 {
     public class MyFirstScript : MonoBehaviour
     {
-        public float a;
-        public float b;
-        public float c;
+        public int a = 100;
+        public int b = 0;
+        public int c = 1;
+        public int d;
 
         void Start()
-        { 
-            float delta = b * b - 4 * a * c;
-            if (delta > 0)
-            {
-                double x1 = (-b + Math.Sqrt(delta)) / (2 * a);
-                double x2 = (-b - Math.Sqrt(delta)) / (2 * a);
+        {
+            //float delta = b * b - 4 * a * c;
+            //if (delta > 0)
+            //{
+            //    double x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+            //    double x2 = (-b - Math.Sqrt(delta)) / (2 * a);
 
-                Debug.Log($"Phương trình có 2 nghiệm : {x1}, {x2}");
-            }
-            else if (delta == 0)
+            //    Debug.Log($"Phương trình có 2 nghiệm : {x1}, {x2}");
+            //}
+            //else if (delta == 0)
+            //{
+            //    double x = -b / (2 * a);
+            //    Debug.Log($"Phương trình có nghiệm kép : {x}");
+            //}
+            //else
+            //{
+            //    Debug.Log("Phương trình vô nghiệm");
+            //}
+
+            int cur = b;
+            int pre = c;
+
+            for (int i = 0; i < a;i++)
             {
-                double x = -b / (2 * a);
-                Debug.Log($"Phương trình có nghiệm kép : {x}");
+               int next = cur + pre;
+
+                if (next > a)
+                {
+                    break;
+                }
+
+                Debug.Log($"{next}");
+                cur = pre;
+                pre = next;
+                
             }
-            else
-            {
-                Debug.Log("Phương trình vô nghiệm");
-            }
+
+
         }
+        public int GetSquareRoot(int a)
+        {
+            for (int i = 0; i <= a; i++)
+            {
+                if (i * i == a)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+        
     }
 }
